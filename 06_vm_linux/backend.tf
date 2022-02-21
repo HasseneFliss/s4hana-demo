4abfbd2f93b5db990992a3eaa39cf5b24e02319f
hassene fliss <hassen.fliss@avaxia-group.com>
2022-02-22 05:03:40 +0900

    init

--- 06_vm_linux/backend.tf
+++ 06_vm_linux/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tf_state"
+    resource_group_name  = "S4HANA"
     storage_account_name = "tfstate019"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
